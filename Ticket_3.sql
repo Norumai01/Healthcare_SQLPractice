f7a7a0b21e9fc4ae84f12966166f4fa60cbad3dd
Norumai01 <johnnydinhnguyen2001@gmail.com>
2024-08-12 13:30:24 -0400

    Initial Project

---
+++ Ticket_3.sql
@@ -0,0 +1,15 @@
+use healthcaresystemdb;
+
+# Ticket #3
+
+Select * From patients
+Order By LastName ASC;
+
+Select * From doctors
+Order By Specialization DESC;
+
+Select * From appointments
+Order By AppointmentDate DESC;
+
+Select * From departments
+Order By DepartmentName ASC;
